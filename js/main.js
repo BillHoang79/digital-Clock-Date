d5ced246c474a5aabbc32219ee78816cdf7f4dcc
bill hoang <billhoang79@gmail.com>
2016-12-06 12:53:22 -0800

    clock with date

--- js/main.js
+++ js/main.js
@@ -1,22 +1,58 @@
-function addItem(){
-	//user types in item to add on list
-	var userInput = document.getElementById('input').value
-	//the item is them taken and stored in text
-	var text = document.createTextNode(userInput)
-	//text becomes li and is ready to be put on list
-	var li = document.createElement('li')
-	//item list is retrieved and li is taken 
-	document.getElementById('items').appendChild(li)
-	//li is added to the list with this 
-	li.appendChild(text)
 
-function removeItem() {
-    li.className = "remove"
+
+//adds 0 infront of the time if hours are between 1-9
+function addZero(t) {
+	if (t < 10) {
+		t = "0" + t
 	}
-	li.onclick = removeItem
+	//returns time back
+	return t
+}
+//runs the clock
+function runClock () {
+	//stores time into today
+	var today = new Date()
+
+	//stores values into vars
+	var hour = today.getHours()
+	var minutes = today.getMinutes()
+	var seconds = today.getSeconds()
+	var ampm = (hour >= 12 ? ' pm' : ' am')
+	//stores values into vars
+	var date = today.getDate()
+	var month = today.getMonth()
+	var year = today.getFullYear()
+	
+	//applies am or pm
+	hour = hour % 12 || 12
+
+	//converts 24 hour clock to 12 hour clock
+	if(hour > 12) {
+		hour = hour - 12
+	}
+	//changes month from 0-11 t0 1-12
+	if(month == 11  || month == 0 ){
+		month++
+	}else {
+		month++
+	}
+
+	//updates minutes and second from line 1
+	hour = addZero(hour)
+	minutes = addZero(minutes)
+	seconds = addZero(seconds)
+
+	//grabs id clock from html and changes text to time visversa for date
+	document.getElementById('clock').textContent = hour + ":" + minutes + ':' + seconds + ampm
+	document.getElementById('date').textContent = month + "/" + date + "/" + year
+
+	//runs the function every second
+	setTimeout(function () {
+		runClock()
+	}, 1000)
 }
 
-
-
-
+//event listener to start function and displays time and date
+document.getElementById('date').addEventListener('onload', runClock(), false)
+document.getElementById('clock').addEventListener('onload', runClock(), false)
 
